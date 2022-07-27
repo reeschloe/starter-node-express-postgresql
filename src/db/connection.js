992f212d5b0be3d0fbd07ccf2c079c658687adc8
Chloe Rees <reeschloe@yahoo.com>
2022-07-27 09:49:39 -0500

    move config to connection.js

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,5 +1,5 @@
-const env = process.env.NODE_ENV || "development";
-const config = require("../../knexfile")[env];
-const knex = require("knex")("config");
+// const env = process.env.NODE_ENV || "development";
+// const config = require("../../knexfile")[env];
+const knex = require("knex")({client: 'postgresql', connection: process.env.DATABASE_URL});
 
 module.exports = knex;
